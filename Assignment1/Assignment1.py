56947cd4bb1504216493bb153c8625db74b84e19
Shashank Kanakapura Srivatsa <srivatsa@mail.uni-paderborn.de>
2020-05-06 20:42:01 +0200

    Correct plot with colours - legends missing, randomly dropping values missing
    Correct plot with colours - legends missing, randomly dropping values missing

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -2,8 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import random
 
-x,y,z,u,v,w = [],[],[],[],[],[]
+x,y,u,v = [],[],[],[]
+randomNumberList = []
+colorList = []
 with open('field2.irreg.txt','r') as fileHandle:
     for i in range(0,6):
         fileHandle.readline()
@@ -13,12 +16,23 @@ for singleLine in inputLine:
     values = singleLine.split(' ')
     x.append(float(values[0]))
     y.append(float(values[1]))
-    z.append(float(values[2]))
     u.append(float(values[3]))
     v.append(float(values[4]))
-    w.append(float(values[5]))
+    colorList.append(np.sqrt(((float(values[3])))**2 +((float(values[4])))**2))
 
-fig, ax = plt.subplots()
+'''
+for i in range(0,100):
+    randomNumberList.append(random.randrange(0,max(len(x),len(y))))
 
-ax.quiver(x,y,u,v)
+for randomNumber in randomNumberList:
+    del x[randomNumber]
+    del y[randomNumber]
+    del u[randomNumber]
+    del v[randomNumber]    
+'''
+fig, ax = plt.subplots(figsize=(7,7))
+ax.quiver(x,y,u,v,colorList)
+ax.xaxis.set_ticks([])
+ax.yaxis.set_ticks([])
+
 plt.show()
